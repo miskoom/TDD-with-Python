c54b3c2f8f32324c0c9cec4a6ebcdfdaf95f0a8e
miskoom <hmiskoom@yahoo.com>
2017-06-12 18:56:06 +0100

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
-	
+
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -15,23 +17,37 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
 
         # A friend is invited to enter a to-do item straight away
-        # [...rest if comments as before]
-    # self.browser.quit()
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+                    'Enter a to-do item'
+        )
+
+        #When she hits enter, the page updates, and now the page lists
+        #"1: Buy peacock feathers" as an item in a to-do lists
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+            )
+
+        #There is still a text box inviting her to add another item. She enters
+        # "Use peacock feathers to make a fly" (Shes very methodical)
+        self.self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
 
 # She types "Buy peacock feathers" into a text box
 
-#When she hits enter, the page updates, and now the page lists
-#"1: Buy peacock feathers" as an item in a to-do lists
 
-#There is still a text box inviting her to add another item. She enters
-# "Use peacock feathers to make a fly" (Shes very methodical)
 
 #The page updates again, and now shows both items on her lists
 
